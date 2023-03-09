b8befa53238c8adcc2c4d1485ccf39d5a9125aee
priscila torres reyes <priscyla.torres16@gmail.com>
2023-03-09 19:51:27 -0300

    add author

--- myApp.js
+++ myApp.js
@@ -1,7 +1,34 @@
+const dotenv = require('dotenv').config();
 let express = require('express');
 let app = express();
 
-console.log("Hello World" )
+app.use((req, res, next) => {
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string)
+    next()
+})
+app.use('/public', express.static(__dirname + '/public'))
+
+
+app.get('/', function (req, res) {
+    res.send("Hello Express")
+})
+
+app.get('/public', function (req, res) {
+    const absolutePath = __dirname + '/views/index.html'
+    res.sendFile(absolutePath);
+})
+
+app.get('/json', (function (req, res) {
+    const mySecret = process.env.MESSAGE_STYLE;
+
+    if (mySecret === "uppercase") {
+        res.json({ "message": "Hello json".toUpperCase() });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+
+}))
 
 
 
@@ -38,4 +65,6 @@ console.log("Hello World" )
 
 
 
- module.exports = app;
+
+
+module.exports = app;
